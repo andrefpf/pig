e2c067deaee4c51d0e7eb8bd1e6a4bfdb6f107ba
André Fernandes <fpf.andre@gmail.com>
2025-06-03 09:54:15 -0300

    add another test for cabac

--- tests/test_cabac.py
+++ tests/test_cabac.py
@@ -2,6 +2,41 @@ from jpig.entropy.cabac import Cabac
 import numpy as np
 
 
+def test_specific_sequence():
+    # random sequence of booleans with more zeros than ones
+    original = [
+        True, True, True, False,
+        True, True, False, True,
+        True, False, True, True,
+        False, True, True, True,
+        True, True, True, False,
+        True, True, True, True,
+        True, True, True, True,
+        False, True, True, True,
+    ]  # fmt: skip
+
+    expected_encoding = [
+        True, True, False, False,
+        False, True, True, True, 
+        True, False, True, False, 
+        False, True, False, True, 
+        True, False, True, False, 
+        False, False, False, False, 
+        True, True
+    ]  # fmt: skip
+
+
+    cabac = Cabac()
+    encoded = cabac.encode(original)
+    decoded = cabac.decode(encoded, len(original))
+
+
+    assert encoded == expected_encoding
+    assert len(encoded) <= len(original)
+    assert original == decoded
+
+
+
 def test_more_zeros_than_ones():
     # random sequence of booleans with more zeros than ones
     original = list(np.random.random(100) < 0.2)
