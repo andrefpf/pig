2814332963f907148b7107e86f906fb92530b4f4
André Fernandes <fpf.andre@gmail.com>
2025-06-03 18:33:08 -0300

    update cabac pytest to consider separated encoder and decoder

--- tests/test_cabac.py
+++ tests/test_cabac.py
@@ -1,16 +1,15 @@
 import numpy as np
 from bitarray import bitarray
 
-from jpig.entropy.cabac import CabacEncoder
+from jpig.entropy.cabac import CabacEncoder, CabacDecoder
 
 
 def test_specific_sequence():
     original = bitarray("1110 1101 1011 0111 1110 1111 1111 0111")
     expected_encoding = bitarray("1100 0111 1010 0101 1010 0000 11")
 
-    cabac = CabacEncoder()
-    encoded = cabac.encode(original)
-    decoded = cabac.decode(encoded, len(original))
+    encoded = CabacEncoder().encode(original)
+    decoded = CabacDecoder().decode(encoded, len(original))
 
     assert encoded == expected_encoding
     assert len(encoded) <= len(original)
@@ -21,9 +20,8 @@ def test_more_zeros_than_ones():
     # random sequence of booleans with more zeros than ones
     original = bitarray((np.random.random(100) < 0.2).tolist())
 
-    cabac = CabacEncoder()
-    encoded = cabac.encode(original)
-    decoded = cabac.decode(encoded, len(original))
+    encoded = CabacEncoder().encode(original)
+    decoded = CabacDecoder().decode(encoded, len(original))
 
     assert len(encoded) <= len(original)
     assert original == decoded
@@ -33,9 +31,8 @@ def test_more_ones_than_zeros():
     # random sequence of booleans with more ones than zeros
     original = bitarray((np.random.random(100) < 0.9).tolist())
 
-    cabac = CabacEncoder()
-    encoded = cabac.encode(original)
-    decoded = cabac.decode(encoded, len(original))
+    encoded = CabacEncoder().encode(original)
+    decoded = CabacDecoder().decode(encoded, len(original))
 
     assert len(encoded) <= len(original)
     assert original == decoded
