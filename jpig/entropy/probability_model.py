075885e706e7cc16540f10a6d3eef3c2b001449b
André Fernandes <fpf.andre@gmail.com>
2025-06-04 10:01:40 -0300

    update probability model to allow saving and retrieving values into a stack

--- jpig/entropy/probability_model.py
+++ jpig/entropy/probability_model.py
@@ -1,18 +1,31 @@
-from dataclasses import dataclass
-import numpy as np
+from dataclasses import dataclass, field
 from typing import Literal
+
+import numpy as np
 
 
 @dataclass
 class ProbabilityModel:
     frequency_of_zeros: int = 1
     frequency_of_ones: int = 1
+    _stack: list[tuple[int, int]] = field(default_factory=list)
 
     def add_bit(self, bit: bool | Literal[0, 1]):
         if bit:
             self.frequency_of_ones += 1
         else:
             self.frequency_of_zeros += 1
+
+    def push(self):
+        self._stack.append((self.frequency_of_zeros, self.frequency_of_ones))
+
+    def pop(self):
+        self.frequency_of_zeros, self.frequency_of_ones = self._stack.pop()
+
+    def clear(self):
+        self.frequency_of_ones = 1
+        self.frequency_of_zeros = 1
+        self._stack.clear()
 
     def total_bits(self) -> int:
         return self.frequency_of_zeros + self.frequency_of_ones
@@ -23,7 +36,7 @@ class ProbabilityModel:
     def probability(self, bit: bool | Literal[0, 1]) -> float:
         if self.frequency(0) <= 0:
             return 0
-    
+
         if self.frequency(1) <= 0:
             return 0
 
@@ -33,8 +46,8 @@ class ProbabilityModel:
             return self.frequency_of_zeros / self.total_bits()
 
     def entropy(self) -> float:
-        prob_0 = self.probability_of_zeros()
-        prob_1 = self.probability_of_ones()
+        prob_0 = self.probability(0)
+        prob_1 = self.probability(1)
 
         if prob_0 == 0:
             return 0
