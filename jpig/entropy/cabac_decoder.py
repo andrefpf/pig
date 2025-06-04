c852a23a2d074050f315529c4a6468059611600f
André Fernandes <fpf.andre@gmail.com>
2025-06-04 10:04:42 -0300

    update cabac decoder to show the decoded bit on each iteration

--- jpig/entropy/cabac_decoder.py
+++ jpig/entropy/cabac_decoder.py
@@ -57,22 +57,26 @@ class CabacDecoder:
         if model is not None:
             self.use_model(model)
 
+        output = None
         self._update_table()
 
         if self.low <= self.current <= self.mid:
             self.high = self.mid
             self.probability_model.add_bit(0)
             self.result.append(0)
+            output = 0
 
         elif self.mid < self.current <= self.high:
             self.low = self.mid + 1
             self.probability_model.add_bit(1)
             self.result.append(1)
+            output = 1
 
         else:
-            print("ENTROU AQUI???")
+            raise ValueError("Invalid encoding sequence.")
 
         self._resolve_scaling()
+        return output
 
     def end(self):
         return self.result
@@ -102,9 +106,7 @@ class CabacDecoder:
                 self.low -= self._half_range + 1
                 self.current -= self._half_range + 1
 
-            elif (self._quarter_range < self.low) and (
-                self.high <= self._three_quarter_range
-            ):
+            elif (self._quarter_range < self.low) and (self.high <= self._three_quarter_range):
                 self.high -= self._quarter_range + 1
                 self.low -= self._quarter_range + 1
                 self.current -= self._quarter_range + 1
