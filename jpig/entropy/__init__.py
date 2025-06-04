f6bb6aef9bbbd3b7b77eea302537002e83925921
André Fernandes <fpf.andre@gmail.com>
2025-06-04 10:09:16 -0300

    update __init__.py files

--- jpig/entropy/__init__.py
+++ jpig/entropy/__init__.py
@@ -3,11 +3,11 @@ from .cabac_encoder import CabacEncoder
 from .cabac_decoder import CabacDecoder
 from .mule_encoder import MuleEncoder
 from .mule_decoder import MuleDecoder
-from .mule import Mule
 
 __all__ = [
     "CabacEncoder",
     "CabacDecoder",
-    "Mule",
+    "MuleEncoder",
+    "MuleDecoder",
     "ProbabilityModel",
 ]
