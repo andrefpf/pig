0f00ef15f154abe7543ebbad29424cb7b54d15b2
André Fernandes <fpf.andre@gmail.com>
2025-06-09 20:11:27 -0300

    fix bug on whole image mule

--- jpig/codecs/whole_image_mule.py
+++ jpig/codecs/whole_image_mule.py
@@ -8,7 +8,7 @@ class WholeImageMule:
     def encode(self, data: np.ndarray, lagrangian: float) -> bitarray:
         transformed: np.ndarray = dctn(data, norm="ortho")
         transformed = transformed.round().astype(int)
-        
+
         max_bitplane = MuleEncoder.find_max_bitplane(transformed)
         mule_encoder = MuleEncoder()
 
@@ -18,13 +18,17 @@ class WholeImageMule:
             header.extend(f"{size:032b}")
         header.extend(f"{max_bitplane:08b}")
 
-        bitstream = mule_encoder.encode(transformed, lagrangian, max_bitplane)
+        bitstream = mule_encoder.encode(
+            transformed,
+            lagrangian,
+            upper_bitplane=max_bitplane,
+        )
         return header + bitstream
 
     def decode(self, codestream: bitarray) -> np.ndarray:
         codestream = codestream.copy()
         mule_decoder = MuleDecoder()
-        
+
         ndim = self._consume_bytes(codestream, 8)
         shape = list()
         for _ in range(ndim):
@@ -32,7 +36,11 @@ class WholeImageMule:
             shape.append(size)
 
         max_bitplane = self._consume_bytes(codestream, 8)
-        transformed_decoded = mule_decoder.decode(codestream, shape, max_bitplane)
+        transformed_decoded = mule_decoder.decode(
+            codestream,
+            shape,
+            upper_bitplane=max_bitplane,
+        )
 
         decoded: np.ndarray = idctn(transformed_decoded, norm="ortho")
         decoded = decoded.round().astype(int)
