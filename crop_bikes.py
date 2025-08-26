241f50839c2e887c29689ee6326dd43bb90f5fcf
André Fernandes <fpf.andre@gmail.com>
2025-08-25 21:18:04 -0300

    add temporary solution to crop bikes image

---
+++ crop_bikes.py
@@ -0,0 +1,12 @@
+from jpig.utils.pgx_handler import PGXHandler
+from PIL import Image
+
+handler = PGXHandler()
+
+array = handler.read("./datasets/images/Bikes/0/000_000.pgx")
+array = array[:432, :624]
+
+handler.write("./datasets/images/Bikes_cropped/0/000_000.pgx", array)
+
+img = Image.fromarray((array >> 2).astype("uint8"))
+img.save("./datasets/images/bikes_cropped.pgm")
