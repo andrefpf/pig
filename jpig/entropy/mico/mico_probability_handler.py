e9d2b2a34f6620e1bb1983f69fbe39db51de35b5
André Fernandes <fpf.andre@gmail.com>
2025-09-10 20:02:30 -0300

    create Mico probability handler

---
+++ jpig/entropy/mico/mico_probability_handler.py
@@ -0,0 +1,46 @@
+from jpig.entropy.probability_model.frequentist_pm import FrequentistPM
+
+
+class MicoProbabilityHandler:
+    def __init__(self):
+        self.signals_probability_model = FrequentistPM()
+        self.integer_probability_models = [FrequentistPM() for _ in range(32)]
+
+        self.unit_flags_model = FrequentistPM()
+        self.split_flags_model = FrequentistPM()
+        self.block_flags_model = FrequentistPM()
+
+    def signal_model(self) -> FrequentistPM:
+        return self.signals_probability_model
+
+    def int_model(self, bitplane: int) -> FrequentistPM:
+        assert 0 <= bitplane < 32
+        return self.integer_probability_models[bitplane]
+
+    def split_model(self) -> FrequentistPM:
+        return self.split_flags_model
+    
+    def block_model(self) -> FrequentistPM:
+        return self.block_flags_model
+    
+    def unit_model(self) -> FrequentistPM:
+        return self.unit_flags_model
+
+    def push(self):
+        for model in self._all_models():
+            model.push()
+
+    def pop(self):
+        for model in self._all_models():
+            model.pop()
+
+    def clear(self):
+        for model in self._all_models():
+            model.clear()
+
+    def _all_models(self):
+        return (
+            self.signals_probability_model,
+            *self.flag_probability_models,
+            *self.integer_probability_models,
+        )
