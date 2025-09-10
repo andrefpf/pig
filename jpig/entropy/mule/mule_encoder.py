e50311786d5a0c8b3514e996f807881c8749c499
André Fernandes <fpf.andre@gmail.com>
2025-09-10 00:28:54 -0300

    Decouple mule encoder from probability model handler

--- jpig/entropy/mule/mule_encoder.py
+++ jpig/entropy/mule/mule_encoder.py
@@ -7,6 +7,8 @@ from jpig.entropy import CabacEncoder, FrequentistPM
 from jpig.metrics import RD, energy
 from jpig.utils.block_utils import split_blocks_in_half
 
+from .mule_probability_handler import MuleProbabilityHandler
+
 
 class MuleEncoder:
     def __init__(self):
@@ -17,10 +19,7 @@ class MuleEncoder:
         self.upper_bitplane = 32
         self.lagrangian = 10_000
 
-        self.signals_probability_model = FrequentistPM()
-        self.flag_probability_models = [FrequentistPM() for _ in range(self.upper_bitplane * 2)]
-        self.bitplane_probability_models = [FrequentistPM() for _ in range(self.upper_bitplane)]
-
+        self.prob_handler = MuleProbabilityHandler()
         self.bitstream = bitarray()
         self.cabac = CabacEncoder()
 
@@ -42,12 +41,12 @@ class MuleEncoder:
         if self.lower_bitplane is None:
             self.lower_bitplane = self._find_optimal_lower_bitplane(block)
 
-        self._clear_models()
+        self.prob_handler.clear()
         self.flags, self.estimated_rd = self._recursive_optimize_encoding_tree(
             block,
             self.upper_bitplane,
         )
-        self._clear_models()
+        self.prob_handler.clear()
 
         self.cabac.start(result=self.bitstream)
         self.encode_int(self.lower_bitplane, 0, 5, signed=False)
@@ -67,18 +66,18 @@ class MuleEncoder:
 
         if flag == "Z":
             # 1
-            self.cabac.encode_bit(1, model=self.flag_probability_models[upper_bitplane * 2])
+            self.cabac.encode_bit(1, model=self.prob_handler.flag_model(upper_bitplane, 0))
 
         elif flag == "L":
             # 00
-            self.cabac.encode_bit(0, model=self.flag_probability_models[upper_bitplane * 2 + 0])
-            self.cabac.encode_bit(0, model=self.flag_probability_models[upper_bitplane * 2 + 1])
+            self.cabac.encode_bit(0, model=self.prob_handler.flag_model(upper_bitplane, 0))
+            self.cabac.encode_bit(0, model=self.prob_handler.flag_model(upper_bitplane, 1))
             self.apply_encoding(flags, block, upper_bitplane - 1)
 
         elif flag == "S":
             # 01
-            self.cabac.encode_bit(0, model=self.flag_probability_models[upper_bitplane * 2 + 0])
-            self.cabac.encode_bit(1, model=self.flag_probability_models[upper_bitplane * 2 + 1])
+            self.cabac.encode_bit(0, model=self.prob_handler.flag_model(upper_bitplane, 0))
+            self.cabac.encode_bit(1, model=self.prob_handler.flag_model(upper_bitplane, 1))
             for sub_block in split_blocks_in_half(block):
                 self.apply_encoding(flags, sub_block, upper_bitplane)
 
@@ -95,11 +94,11 @@ class MuleEncoder:
         absolute = np.abs(value)
         for i in range(lower_bitplane, upper_bitplane):
             bit = ((1 << i) & absolute) != 0
-            self.cabac.encode_bit(bit, model=self.bitplane_probability_models[i])
+            self.cabac.encode_bit(bit, model=self.prob_handler.int_model(i))
 
         mask = (1 << lower_bitplane) - 1
         if signed and (absolute & ~mask) != 0:
-            self.cabac.encode_bit(value < 0, model=self.signals_probability_model)
+            self.cabac.encode_bit(value < 0, model=self.prob_handler.signal_model())
 
     def _find_optimal_lower_bitplane(self, block: np.ndarray) -> int:
         lower_bitplane = 0
@@ -110,7 +109,7 @@ class MuleEncoder:
         for i in reversed(range(0, self.upper_bitplane)):
             bit_position = 1 << i
             mask = bit_position - 1
-            model = self.bitplane_probability_models[i]
+            model = self.prob_handler.int_model(i)
             non_zeroed = magnitudes > bit_position
 
             bits_to_encode = magnitudes[non_zeroed] & bit_position != 0
@@ -143,7 +142,7 @@ class MuleEncoder:
             rd = self._estimate_integer(block, self.lower_bitplane, upper_bitplane)
             return "", rd
 
-        self._push_models()
+        self.prob_handler.push()
         should_lower_bitplane = self.is_bitplane_zero(block, upper_bitplane)
         if should_lower_bitplane:
             segmentation_flags, segmentation_rd = self._estimate_lower_bp_flag(
@@ -157,20 +156,20 @@ class MuleEncoder:
             )
 
         zero_rd = RD(
-            rate=self.flag_probability_models[upper_bitplane * 2].estimate_bit(1),
+            rate=self.prob_handler.flag_model(upper_bitplane, 0).estimate_bit(1),
             distortion=energy(block),
         )
 
         if segmentation_rd.cost(self.lagrangian) < zero_rd.cost(self.lagrangian):
             return segmentation_flags, segmentation_rd
         else:
-            self._pop_models()
+            self.prob_handler.pop()
             return self._estimate_zero_flag(block, upper_bitplane)
 
     def _estimate_zero_flag(self, block: np.ndarray, upper_bitplane: int) -> tuple[str, RD]:
         rd = RD()
         rd.distortion = energy(block)
-        rd.rate += self.flag_probability_models[upper_bitplane * 2].add_and_estimate_bit(1)
+        rd.rate += self.prob_handler.flag_model(upper_bitplane, 0).add_and_estimate_bit(1)
         return "Z", rd
 
     def _estimate_lower_bp_flag(self, block: np.ndarray, upper_bitplane: int) -> tuple[str, RD]:
@@ -181,8 +180,8 @@ class MuleEncoder:
         rd = RD()
 
         for _ in range(number_of_flags):
-            rd.rate += self.flag_probability_models[upper_bitplane * 2 + 0].add_and_estimate_bit(0)
-            rd.rate += self.flag_probability_models[upper_bitplane * 2 + 1].add_and_estimate_bit(0)
+            rd.rate += self.prob_handler.flag_model(upper_bitplane, 0).add_and_estimate_bit(0)
+            rd.rate += self.prob_handler.flag_model(upper_bitplane, 1).add_and_estimate_bit(0)
 
         current_flags, current_rd = self._recursive_optimize_encoding_tree(
             block,
@@ -202,8 +201,8 @@ class MuleEncoder:
         rd = RD()
         flags = "S"
 
-        rd.rate += self.flag_probability_models[upper_bitplane * 2 + 0].add_and_estimate_bit(0)
-        rd.rate += self.flag_probability_models[upper_bitplane * 2 + 1].add_and_estimate_bit(1)
+        rd.rate += self.prob_handler.flag_model(upper_bitplane, 0).add_and_estimate_bit(0)
+        rd.rate += self.prob_handler.flag_model(upper_bitplane, 1).add_and_estimate_bit(1)
 
         for sub_block in split_blocks_in_half(block):
             current_flags, current_rd = self._recursive_optimize_encoding_tree(
@@ -230,38 +229,13 @@ class MuleEncoder:
 
         for i in range(lower_bitplane, upper_bitplane):
             bit = (1 << i) & masked_value != 0
-            model = self.bitplane_probability_models[i]
+            model = self.prob_handler.int_model(i)
             rd.rate += model.add_and_estimate_bit(bit)
 
         if masked_value != 0:
-            rd.rate += self.signals_probability_model.add_and_estimate_bit(value < 0)
+            rd.rate += self.prob_handler.signal_model().add_and_estimate_bit(value < 0)
 
         return rd
-
-    def _estimate_current_rate(self) -> float:
-        total_size = 0
-        for model in self.probability_models():
-            total_size += model.total_estimated_rate()
-        return total_size
-
-    def _push_models(self):
-        for model in self.probability_models():
-            model.push()
-
-    def _pop_models(self):
-        for model in self.probability_models():
-            model.pop()
-
-    def _clear_models(self):
-        for model in self.probability_models():
-            model.clear()
-
-    def probability_models(self):
-        return [
-            self.signals_probability_model,
-            *self.flag_probability_models,
-            *self.bitplane_probability_models,
-        ]
 
     @staticmethod
     def find_max_bitplane(block: np.ndarray):
---
+++ jpig/entropy/mule/mule_probability_handler.py
@@ -0,0 +1,36 @@
+from jpig.entropy.probability_model.frequentist_pm import FrequentistPM
+
+
+class MuleProbabilityHandler:
+    def __init__(self):
+        self.signals_probability_model = FrequentistPM()
+        self.flag_probability_models = [FrequentistPM() for _ in range(32 * 2)]
+        self.integer_probability_models = [FrequentistPM() for _ in range(32)]
+
+    def signal_model(self) -> FrequentistPM:
+        return self.signals_probability_model
+
+    def int_model(self, bitplane: int) -> FrequentistPM:
+        return self.integer_probability_models[bitplane]
+
+    def flag_model(self, bitplane: int, position: int) -> FrequentistPM:
+        return self.flag_probability_models[bitplane * 2 + position]
+
+    def push(self):
+        for model in self._all_models():
+            model.push()
+
+    def pop(self):
+        for model in self._all_models():
+            model.pop()
+
+    def clear(self):
+        for model in self._all_models():
+            model.clear()
+
+    def _all_models(self):
+        return (
+            self.signals_probability_model,
+            *self.flag_probability_models,
+            *self.integer_probability_models,
+        )
