fb912434043784786f75339d54e4e4d7941736cd
André Fernandes <fpf.andre@gmail.com>
2025-09-11 12:39:00 -0300

    refactor MuleProbabilityHandler to set parameters as "private"

--- jpig/entropy/mule/mule_probability_handler.py
+++ jpig/entropy/mule/mule_probability_handler.py
@@ -3,21 +3,21 @@ from jpig.entropy.probability_model.frequentist_pm import FrequentistPM
 
 class MuleProbabilityHandler:
     def __init__(self):
-        self.signals_probability_model = FrequentistPM()
-        self.flag_probability_models = [FrequentistPM() for _ in range(32 * 2)]
-        self.integer_probability_models = [FrequentistPM() for _ in range(32)]
+        self._signals_probability_model = FrequentistPM()
+        self._flag_probability_models = [FrequentistPM() for _ in range(32 * 2)]
+        self._integer_probability_models = [FrequentistPM() for _ in range(32)]
 
     def signal_model(self) -> FrequentistPM:
-        return self.signals_probability_model
+        return self._signals_probability_model
 
     def int_model(self, bitplane: int) -> FrequentistPM:
         assert 0 <= bitplane < 32
-        return self.integer_probability_models[bitplane]
+        return self._integer_probability_models[bitplane]
 
     def flag_model(self, bitplane: int, position: int) -> FrequentistPM:
         assert position in (0, 1)
         assert 0 <= bitplane < 32
-        return self.flag_probability_models[bitplane * 2 + position]
+        return self._flag_probability_models[bitplane * 2 + position]
 
     def push(self):
         for model in self._all_models():
@@ -33,7 +33,7 @@ class MuleProbabilityHandler:
 
     def _all_models(self):
         return (
-            self.signals_probability_model,
-            *self.flag_probability_models,
-            *self.integer_probability_models,
+            self._signals_probability_model,
+            *self._flag_probability_models,
+            *self._integer_probability_models,
         )
