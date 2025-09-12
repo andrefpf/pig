c975876477f1261a04ba9d7ffa46c7d897cc137b
André Fernandes <fpf.andre@gmail.com>
2025-09-12 11:03:44 -0300

    Update main testing mico

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fft import dctn, idctn
 
 from jpig.codecs import BlockedMicoQuantized, BlockedMule
+from jpig.entropy.mico.mico_decoder import MicoDecoder
+from jpig.entropy.mico.mico_encoder import MicoEncoder
+from jpig.entropy.mico.mico_optimizer import MicoOptimizer
+from jpig.entropy.mule.mule_optimizer import MuleOptimizer
 from jpig.media import RawImage
 from jpig.metrics import mse, psnr
+from jpig.utils.block_utils import bigger_possible_slice
 
 
-def compare_data(data1, data2):
+def compare_data(data1, data2, bitdepth=8):
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
     axes[0].imshow(
         data1,
         vmin=0,
-        vmax=(1 << 8),
+        vmax=(1 << bitdepth),
         cmap="gray",
     )
     axes[0].axis("off")
@@ -20,7 +27,7 @@ def compare_data(data1, data2):
     axes[1].imshow(
         data2,
         vmin=0,
-        vmax=(1 << 8),
+        vmax=(1 << bitdepth),
         cmap="gray",
     )
     axes[1].axis("off")
@@ -30,37 +37,22 @@ def compare_data(data1, data2):
 
 
 def main():
-    img = RawImage.from_file("./datasets/images/cameraman.pgm")
-    block_size = 8
+    image = RawImage.from_file("./datasets/images/Bikes_64x64/0/000_000.pgx")[16:32, 16:32]
+    frequency = dctn(image, norm="ortho").astype(int)
+
+    encoder = MicoEncoder()
+    encoded = encoder.encode(frequency, 1_000)
+
+    decoder = MicoDecoder()
+    decoded = decoder.decode(encoded, frequency.shape)
+    reconstructed = idctn(decoded, norm="ortho").astype(int)
     
-    codec = BlockedMicoQuantized()
-    
-    from time import perf_counter
-    start = perf_counter()
-    bitstream = codec.encode(img, 1e-8, 90, block_size)
-    end = perf_counter()
-    print("Time: ", end - start)
+    print()
+    print(f"PSNR: {psnr(image, reconstructed, 10)}")
+    print(f"MSE: {mse(image, reconstructed)}")
+    print(f"BPP:", len(encoded) / image.size)
 
-
-    decoded = codec.decode(bitstream)
-
-    print(f"Original Rate: {img.number_of_samples() * img.bitdepth / 8000:.2f} Kb")
-    print(f"Rate: {len(bitstream) / 8000:.2f} Kb")
-    print(f"MSE: {mse(img, decoded)}")
-    print(f"PSNR: {psnr(img, decoded, img.bitdepth)}")
-    # compare_data(img, decoded)
-
-    print("-" * 10)
-
-    codec = BlockedMule()
-    bitstream = codec.encode(img, 40, block_size)
-    decoded = codec.decode(bitstream)
-
-    print(f"Original Rate: {img.number_of_samples() * img.bitdepth / 8000:.2f} Kb")
-    print(f"Rate: {len(bitstream) / 8000:.2f} Kb")
-    print(f"MSE: {mse(img, decoded)}")
-    print(f"PSNR: {psnr(img, decoded, img.bitdepth)}")
-    # compare_data(img, decoded)
+    compare_data(image, reconstructed, 10)
 
 
 if __name__ == "__main__":
